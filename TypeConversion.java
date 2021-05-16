1c539d9fbe8f9ff77aac7bd09e5cb732e9b98854
aksh-51n9h <singh2104.akshay@gmail.com>
2021-05-16 18:05:57 +0530

    Type Conversion and Operators

---
+++ TypeConversion.java
@@ -0,0 +1,8 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        // boolean -> int
+        boolean b = true;
+        int i = (int) b;
+        System.out.println(i);
+    }
+}
