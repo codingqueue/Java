f1daabf8cd287badc3b4e05ca5a7e194e0860d37
aksh-51n9h <singh2104.akshay@gmail.com>
2021-05-24 09:09:46 +0530

    if elseif else

--- IfElseIfElseStatement.java
+++ IfElseIfElseStatement.java
@@ -3,15 +3,29 @@ import java.util.Scanner;
 public class IfElseIfElseStatement{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int age = in.nextInt();
+        System.out.println("Enter your percentage: ");
+        double percent = in.nextDouble();
 
-        if(age>18){
-            System.out.println("You're allowed to give vote.");
-        }else if(age==17){
-            System.out.println("You'll be eligible next year for voting.");
-        }else{
-            System.out.println("You're not allowed to give vote.");
+        //print Grade A - >= 90
+        //print Grade B - >=70 && <90
+        //print Grade C - >=40 && <70
+        //print Fail - <40
+
+        if(percent >=90){
+            System.out.println("Grade A");
         }
+        else if(percent>=70 && percent<90)
+        {
+            System.out.println("Grade B");
+        }
+        else if(percent>=40 && percent<70)
+        {
+            System.out.println("Grade C");
+        }
+        else{
+            System.out.println("FAIL");
+        }
+
+        System.out.println("Program ended.");
     }
 }
