3473f0d4997c079031abec2882d52b4737543471
aksh-51n9h <singh2104.akshay@gmail.com>
2021-05-26 17:55:16 +0530

    nested if

---
+++ if_statements/NestedIf.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NestedIf{
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 10;
+        int z = 18;
+
+        if(x>y){
+            System.out.println("x is greater than y");
+        
+            if(x>z){
+                System.out.println("x is the greatest element.");
+            }else{
+                System.out.println("z is the greatest element.");
+            }
+
+        }
+        else{
+            System.out.println("y is greater than x");
+
+            if(y>z){
+                System.out.println("y is the greatest element.");
+            }else{
+                System.out.println("z is the greatest element.");
+            }
+        }
+    }
+}
